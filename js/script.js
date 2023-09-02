da2f7f7c2955e0b6b88fe0866ba07d8b0e1c0a27
Nazmul Hossen <101635882+nazmulhossen1@users.noreply.github.com>
2023-09-02 14:48:55 +0600

    fixed responsive problem

--- js/script.js
+++ js/script.js
@@ -1,51 +1,51 @@
 
 // Blog button
-document.getElementById("openButton").addEventListener("click", function() {
-    window.open("answer.html", "_blank");
-  });
-  
-  
-  const handleCategory = async () => {
-      const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
-      const data = await res.json()
-      const tubeCetagory = data.data;
-    //   console.log(tubeCetagory);
-  
-      const tabContainer = document.getElementById('tap-container')
-      tubeCetagory.forEach((category) => {
-          const div = document.createElement('div')
-          div.innerHTML = `
+document.getElementById("openButton").addEventListener("click", function () {
+  window.open("answer.html", "_blank");
+});
+
+
+const handleCategory = async () => {
+  const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
+  const data = await res.json()
+  const tubeCetagory = data.data;
+  //   console.log(tubeCetagory);
+
+  const tabContainer = document.getElementById('tap-container')
+  tubeCetagory.forEach((category) => {
+    const div = document.createElement('div')
+    div.innerHTML = `
           <a onclick="handleLoadVideo('${category.category_id}')" class="tab bg-[#25252533] px-4 py-1 rounded-sm text-md font-semibold">${category.category}</a>
           `
-          tabContainer.appendChild(div)
-  
-      });
-  }
-  
-  const handleLoadVideo = async (categoryId = 1000) => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
-    const data = await res.json();
-    const videoData = data?.data;
-    // console.log(videoData);
-    const errorComtainer = document.getElementById('error-container');
-        errorComtainer.innerHTML = ''
-    const cardContainer = document.getElementById('card-container');
-    cardContainer.innerHTML = '';
-    
-  
-    if ( videoData.length > 0) {
-        videoData.forEach((video) => {
-            const div = document.createElement('div');
-            div.classList = ` card  card-compact  h-80 bg-base-100 shadow-sm `;
-            div.innerHTML = `
-            <img class="  h-40 rounded-t-md" src= ${video.thumbnail} /> 
-            <span class="relative bottom-[24px] left-[244px] md:bottom-[24px] md:left-[200px] lg:bottom-[24px] lg:left-[150px] bg-[#171717] w-32 text-center text-[12px] text-white rounded-sm">${
-              video.others.posted_date
-                ? convertSecondsToHoursAndMinutes(video.others.posted_date)
-                : ''
-            }</span>
-  
-              <div class="flex items-start gap-4 mt-4 p-2">
+    tabContainer.appendChild(div)
+
+  });
+}
+
+const handleLoadVideo = async (categoryId = 1000) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
+  const data = await res.json();
+  const videoData = data?.data;
+  // console.log(videoData);
+  const errorComtainer = document.getElementById('error-container');
+  errorComtainer.innerHTML = ''
+  const cardContainer = document.getElementById('card-container');
+  cardContainer.innerHTML = '';
+
+
+  if (videoData.length > 0) {
+    videoData.forEach((video) => {
+      const div = document.createElement('div');
+      div.innerHTML = `
+          <div class="card bg-base-100 shadow-sm ">
+            <figure class="relative">
+              <img class="w-full h-40 rounded-t-md" src="${video.thumbnail}" />
+              <span class="absolute bottom-0 right-0 bg-[#171717]  w-32 text-center text-[12px] text-white mb-1 mr-1 rounded-md">${video.others.posted_date ? postdate(video.others.posted_date) : ''}</span>
+            </figure>
+
+      
+          
+          <div class="flex items-start gap-4 mt-4 p-2">
                       <img class=" mt-2 w-10 h-10 rounded-full"
                         src="${video.authors[0]?.profile_picture}"
                         alt="Shoes" />
@@ -53,43 +53,43 @@ document.getElementById("openButton").addEventListener("click", function() {
                           <h3 class="text-md font-bold mt-2">${video.title}</h3>
                       <div class=" flex gap-2 justify-start items-center">
                             <h4 class="mt-2">${video.authors[0]?.profile_name}</h4>
-                            <img class="w-4 h-4 rounded-full mt-2" src="${video.authors[0]?.verified  ? 'images/varify.png' : ''}">
+                            <img class="w-4 h-4 rounded-full mt-2" src="${video.authors[0]?.verified ? 'images/varify.png' : 'hidden'}">
                       </div>
-                        <h4 class="mt-2">${video.others.views ? video.others.views : 'no views'} </h4>
+                        <h4 class="mt-2">${video.others.views ? video.others.views : 'no views'} Views</h4>
                       </div>
                 </div>
-            `;
-            cardContainer.appendChild(div);
-            
-            function convertSecondsToHoursAndMinutes(seconds) {
-              const days = Math.floor(seconds / 86400); // 86400 seconds in a day
-              const hours = Math.floor((seconds % 86400) / 3600);
-              const minutes = Math.floor((seconds % 3600) / 60);
-              
-              let result = '';
-              
-              if (days > 0) {
-                result += `${days}day `;
-              }
-              
-              if (hours > 0) {
-                result += `${hours}hrs `;
-              }
-              
-              if (minutes > 0) {
-                result += `${minutes}min`;
-              }
-              
-              return result.trim(); // Remove trailing space
-            }
-            
-            
-        });
-        const errorComtainer = document.getElementById('error-container');
-        errorComtainer.innerHTML = ''
-    } else {
-        
-        errorComtainer.innerHTML = `
+          `;
+      cardContainer.appendChild(div);
+
+      function postdate(seconds) {
+        const days = Math.floor(seconds / 86400); // 86400 seconds in a day
+        const hours = Math.floor((seconds % 86400) / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+
+        let result = '';
+
+        if (days > 0) {
+          result += `${days}day `;
+        }
+
+        if (hours > 0) {
+          result += `${hours}hrs `;
+        }
+
+        if (minutes > 0) {
+          result += `${minutes}min`;
+        }
+
+        return result.trim(); // Remove trailing space
+      }
+
+
+    });
+    const errorComtainer = document.getElementById('error-container');
+    errorComtainer.innerHTML = ''
+  } else {
+
+    errorComtainer.innerHTML = `
       <div class="flex justify-center items-center mt-24">
         <div class="mx-auto  text-center">
           <img class="mx-auto" src="./images/icon.png" alt="Icon">
@@ -97,17 +97,16 @@ document.getElementById("openButton").addEventListener("click", function() {
         </div>
       </div>
       `;
-      
-    }
-    
-  };
-  
-  
-  
-  
-  handleCategory();
-  handleLoadVideo();
-  
-  
-  
-  
+
+  }
+
+};
+
+
+
+
+handleCategory();
+handleLoadVideo();
+
+
+
